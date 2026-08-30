c548055873ce95d1fbaa0928f8df72bdefc907e9
Faria Arabekova <arabekovaf3@gmail.com>
2026-08-30 23:07:05 +0600

    Fix Bitcoin transaction tracking

--- blockchain/bitcoin.py
+++ blockchain/bitcoin.py
@@ -3,7 +3,6 @@ import requests
 
 def get_transactions(address):
     url = f"https://blockstream.info/api/address/{address}/txs"
-
     response = requests.get(url)
 
     if response.status_code != 200:
@@ -14,7 +13,43 @@ def get_transactions(address):
     if len(transactions) == 0:
         return None
 
-    return transactions[0]
+    tx = transactions[0]
+
+    received = sum(
+        output["value"]
+        for output in tx.get("vout", [])
+        if output.get("scriptpubkey_address") == address
+    )
+
+    spent = sum(
+        input_data.get("prevout", {}).get("value", 0)
+        for input_data in tx.get("vin", [])
+        if input_data.get("prevout", {}).get("scriptpubkey_address") == address
+    )
+
+    value = (received - spent) / 100000000
+
+    from_address = ""
+    if tx.get("vin"):
+        from_address = tx["vin"][0].get("prevout", {}).get(
+            "scriptpubkey_address", ""
+        )
+
+    to_address = ""
+    if tx.get("vout"):
+        to_address = tx["vout"][0].get("scriptpubkey_address", "")
+
+    timestamp = ""
+    if tx.get("status", {}).get("confirmed"):
+        timestamp = tx["status"].get("block_time", "")
+
+    return {
+        "hash": tx.get("txid", ""),
+        "value": value,
+        "from": from_address,
+        "to": to_address,
+        "timeStamp": timestamp,
+    }
 def get_balance(address):
     url = f"https://blockstream.info/api/address/{address}"
 
